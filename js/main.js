084befa77963699f8b1ab86a01fdb7f4e38b871a
vcrepa <victor.crepaldicorrea@gmail.com>
2025-12-27 03:02:33 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,3 +2,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Sistema carregado com sucesso.");
 });
+
+
+fetch("data/artes.json")
+  .then(response => response.json())
+  .then(artes => {
+    const galeria = document.getElementById("galeria");
+
+    artes.forEach(arte => {
+      const item = document.createElement("div");
+      item.className = "thumb";
+
+      item.innerHTML = `
+        <img src="${arte.thumb}" alt="${arte.titulo}">
+        <span>${arte.titulo}</span>
+      `;
+
+      item.onclick = () => abrirImagem(arte.full);
+      galeria.appendChild(item);
+    });
+  });
+
+function abrirImagem(src) {
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  modal.innerHTML = `<img src="${src}">`;
+  modal.onclick = () => modal.remove();
+  document.body.appendChild(modal);
+}
